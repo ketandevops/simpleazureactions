8b0efe47566e5367a18f4c5433b397d3d20deaf5
ketandevops <77294978+ketandevops@users.noreply.github.com>
2025-10-14 14:00:42 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "TerraformDemo"
+    storage_account_name = "terraformcodeops11"
+    container_name       = "tfstatefile"
+    key                  = "simple.terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup99"
+  location = "eastus"
+}
